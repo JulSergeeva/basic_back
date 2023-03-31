195b0e4dd67f9d222eda414187a668063b3492f9
Julia Sergeeva <ysergeeva@taxdome.com>
2023-03-31 12:05:43 +0200

    change migration

--- db/migrate/20230330184023_create_posts.rb
+++ db/migrate/20230330184023_create_posts.rb
@@ -1,12 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
-    create_table :posts, :id => false do |t|
-      t.integer :id
+    create_table :posts do |t|
       t.text :body
       t.integer :user_id
-      #t.datetime :created_at
       t.datetime :archived_at
-      #t.datetime :updated_at
       t.datetime :deleted_at
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_26_121503) do
-  create_table "posts", id: false, force: :cascade do |t|
-    t.integer "id"
+ActiveRecord::Schema[7.0].define(version: 2023_03_30_191006) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
     t.datetime "archived_at"
@@ -21,12 +23,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_26_121503) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", id: false, force: :cascade do |t|
-    t.integer "id"
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
