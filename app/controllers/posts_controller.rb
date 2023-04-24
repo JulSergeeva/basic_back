401b685c45aeffaf3ff0bea6c7e92646579a70f2
Julia Sergeeva <ysergeeva@taxdome.com>
2023-04-24 08:48:03 +0200

    unfinished

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,45 @@ class PostsController < ApplicationController
     p @posts
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
   def show
     @post = Post.find(params[:id])
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to post_path
+  end
+
+  private
+  def post_params
+    params.require(:posts).permit(:title, :body)
   end
 end
